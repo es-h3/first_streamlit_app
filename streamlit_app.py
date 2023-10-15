d104e6a1d9c7d1a92f09cdb0167f3c7ec0b5d050
es-h3 <es-h3@online.no>
2023-10-15 11:05:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas as pd
 
 my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list.set_index("Fruit")
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -10,5 +11,5 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.multiselect("Pick som fruits:",list(my_fruit_list.iloc[:,0]))
+streamlit.multiselect("Pick som fruits:",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
