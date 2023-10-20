f6aa3a68bf64e4550ad48ab346a13f7000ad0d4d
es-h3 <es-h3@online.no>
2023-10-20 06:56:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑Avocado Toast')
+streamlit.text('Dette er en test')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 sel=streamlit.multiselect("Pick som fruits:",list(my_fruit_list.index),["Avocado","Strawberries"])
