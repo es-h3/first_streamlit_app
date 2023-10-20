8f3c55910dbc58444d57cce55eb9b3a756922615
es-h3 <es-h3@online.no>
2023-10-20 07:06:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ if len(sel)>0:
   streamlit.dataframe(my_fruit_list.loc[sel])
 streamlit.header("Fruityvice Fruit Advice")
 fruit_choice=streamlit.text_input("What fruit would you like information about?","orange")
-streamlit.text(f"The user entered:{fruit_choice}"
+streamlit.text(f"The user entered:{fruit_choice}")
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 fruityvice_normalized=pd.json_normalize(fruityvice_response.json())
