8d0a2926322441c22fab2e00f0744ba22b19104a
es-h3 <es-h3@online.no>
2023-10-23 06:44:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,6 @@ my_cur=my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row=my_cur.fetchall()
-streamlit.subheader("The fruit load list contains:")
+streamlit.subheader(f"The fruit load list contains:{type_my_data_row}")
 streamlit.dataframe(my_data_row)
 
