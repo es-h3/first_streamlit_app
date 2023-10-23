bc15b1eb274cc5d2c5e3c57ad7bcf763eb5f0314
es-h3 <es-h3@online.no>
2023-10-23 09:12:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,6 +51,7 @@ def get_fruit_load_list():
 streamlit.subheader(f"The fruit load list contains")
 
 if streamlit.button("Get Fruit Load List"):
+  streamlit.text("Dette var gøy")
   my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_cur,my_data_rows=get_fruit_load_list()
   column_names = [desc[0] for desc in my_cur.description]
