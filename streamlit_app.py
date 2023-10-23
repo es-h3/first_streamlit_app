5cc34bab774f5e7e67c2f87fca0dc88c4d6410be
es-h3 <es-h3@online.no>
2023-10-23 07:59:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,13 @@ if len(sel)>0:
   streamlit.dataframe(my_fruit_list.loc[sel])
 
 
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+    #streamlit.text(fruityvice_response.json())
+    fruityvice_normalized=pd.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+  
+
 streamlit.header("Fruityvice Fruit Advice")
 try:
   fruit_choice=streamlit.text_input("What fruit would you like information about?")
@@ -29,10 +36,7 @@ try:
     streamlit.error("Please select a fruit to get information")
   #streamlit.write(f"The user entered:{fruit_choice}")
   else:
-    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-    #streamlit.text(fruityvice_response.json())
-    fruityvice_normalized=pd.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(get_fruityvice_data(fruit_choice)
 except URLError as e:
   streamlit.error()
 streamlit.stop()
