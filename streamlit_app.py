ee766c666c5e8dc27d87099e6479e49ea64563b2
es-h3 <es-h3@online.no>
2023-10-23 07:54:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,11 +20,13 @@ selec=', '.join(sel)
 #streamlit.text(type(sel))
 if len(sel)>0:
   streamlit.dataframe(my_fruit_list.loc[sel])
+
+
 streamlit.header("Fruityvice Fruit Advice")
 try:
   fruit_choice=streamlit.text_input("What fruit would you like information about?")
   if not fruit_choice:
-    streamlit.error("Please select a fruit to get information
+    streamlit.error("Please select a fruit to get information")
   #streamlit.write(f"The user entered:{fruit_choice}")
   else:
     fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
